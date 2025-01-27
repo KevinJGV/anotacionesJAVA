5c0d13da40a1dd59e467ce7fe560add4af4d26d1
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-01-27 16:58:13 -0500

    feat: base

---
+++ src/test/java/com/c3/anotaciones/AnotacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c3.anotaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
