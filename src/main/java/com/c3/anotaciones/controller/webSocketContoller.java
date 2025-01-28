59382ce549046171903f0c836fc9d6e3e7d3156a
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-01-27 20:28:21 -0500

    feat: completado con websockets

--- src/main/java/com/c3/anotaciones/controller/webSocketContoller.java
+++ src/main/java/com/c3/anotaciones/controller/webSocketContoller.java
@@ -2,6 +2,8 @@ package com.c3.anotaciones.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -9,12 +11,24 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class webSocketContoller extends TextWebSocketHandler {
 
+    private Timer timer;
+
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        LocalDateTime now = LocalDateTime.now();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        session.sendMessage(new TextMessage("Actualmente es: " + now.format(formatter)));
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    LocalDateTime now = LocalDateTime.now();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    String serverMessage = "Actualmente es: " + now.format(formatter);
+        
+                    session.sendMessage(new TextMessage(serverMessage));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 1000);
     }
 }
