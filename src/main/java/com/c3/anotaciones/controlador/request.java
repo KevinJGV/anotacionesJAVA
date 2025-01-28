ecf5f4f96318a4651ee4d95c63393dade03595dd
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-01-27 20:42:15 -0500

    save

---
+++ src/main/java/com/c3/anotaciones/controlador/request.java
@@ -0,0 +1,21 @@
+package com.c3.anotaciones.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.c3.anotaciones.entidades.producto;
+
+
+@RestController
+public class request {
+
+    @GetMapping("/api/producto")
+    @ResponseBody
+    public producto getMethodName() {
+        producto producto = new producto("Leche asada", 5000, "Leche asada de 250g marca ACME");
+        return producto;
+    }
+    
+}
---
+++ src/main/java/com/c3/anotaciones/entidades/producto.java
@@ -0,0 +1,14 @@
+package com.c3.anotaciones.entidades;
+
+public class producto {
+    private String nombre;
+    private int precio;
+    private String descripcion;
+    
+    public producto(String nombre, int precio, String descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+}
